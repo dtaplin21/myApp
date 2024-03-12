d0076da64cc8de51e75996b799a5f10be6b0c7a3
dominique Taplin <Dtaplin21@aol.com>
2024-03-12 08:37:12 -0700

    second

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";
+
+import MobileSidebar from "@/components/mobile-sidebar";
+
 
 const NavBar = () => {
 return (
     <div className="flex items-ceenter p-4">
-        <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu />
-        </Button>
+        <MobileSidebar />
         <div className="flex w-full justify-end">
             <UserButton afterSignOutUrl="/" />
         </div>
